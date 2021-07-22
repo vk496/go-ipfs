b4c105b5bd85a7ab343abeb85e335f48df3dac68
Steven Allen <steven@stebalien.com>
2021-07-22 15:36:32 -0700

    fix: remove some deprecated calls

--- core/corehttp/logs.go
+++ core/corehttp/logs.go
@@ -50,7 +50,7 @@ func LogOption() ServeOption {
 			w.WriteHeader(200)
 			wnf, errs := newWriteErrNotifier(w)
 			lwriter.WriterGroup.AddWriter(wnf)
-			log.Event(n.Context(), "log API client connected") //nolint deprecated
+			log.Info("log API client connected")
 			<-errs
 		})
 		return mux, nil
